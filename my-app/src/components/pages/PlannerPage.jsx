e5ff96977077b7818ea2f942cfffa91e3bd4ae24
Lagufiesta <120291129+Lagufiesta@users.noreply.github.com>
2025-09-19 21:07:30 +0900

    Add files via upload

--- my-app/src/components/pages/PlannerPage.jsx
+++ my-app/src/components/pages/PlannerPage.jsx
@@ -2,25 +2,35 @@ import React, { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import PlannerHeaderWM from "../planner/PlannerHeaderWM";
 //import CalendarGrid from "../indiplanner/CalendarGrid";
-import PlannerSidebar from "../planner/PlannerSidebar";
+import PlannerSidebar from "../layout/PlannerSidebar";
 import { Calendar, CheckSquare, ChevronRight } from "lucide-react";
 import TodoList from "../planner/TodoList";
 import { useCalendar } from "../../hooks/useCalendar";
+import { useScheduleFilter } from "../../hooks/useScheduleFilter";
+import ScheduleModal from "../planner/ScheduleModal";
 
 const PlannerPage = () => {
   // 기본 상태들
-
+  //const initialPlanner=1001;
+  //const [defaultPlanner,setDefaultPlanner] = useState(initialPlanner);
   const [viewMode, setViewMode] = useState("monthly"); // 'monthly' or 'weekly
   const [plannerSidebarOpen, setPlannerSidebarOpen] = useState(true);
-
+  const [nowPlanner, setNowPlanner] = useState(1001); //일정 필터링 테스트용 하드코딩⚠️나중에 바꿔야됨요
+  const [openScModal, setOpenScModal] = useState(false);
+  const [selectedSc, setSelectedSc] = useState(null); //현재 선택된 스케쥴을 나타냄
   //날짜 설정
 
   const calendar = useCalendar();
 
+  const scheduleFilter = useScheduleFilter({
+    nowPlanner,
+    currentDate: calendar.currentDate,
+  });
   return (
     <div className="flex flex-1 flex-col">
       <div className="flex flex-1 overflow-y-auto">
         <button
+          //사이드바 접었다 폈다
           onClick={() => setPlannerSidebarOpen(!plannerSidebarOpen)}
           className="text-md bg-orange-300"
         >
@@ -35,7 +45,7 @@ const PlannerPage = () => {
           //<PlannerSidebar className="" viewMode={viewMode} >
 
           <PlannerSidebar
-            className=""
+            sidebarClassName="md:block hidden"
             viewMode={viewMode}
             // year={calendar.year}
             //month={calendar.month}
@@ -44,6 +54,11 @@ const PlannerPage = () => {
             weeks={calendar.weeks}
             setViewMode={setViewMode}
             weekNames={calendar.weekNames}
+            nowPlanner={nowPlanner}
+            openScModal={openScModal}
+            setOpenScModal={setOpenScModal}
+            selectedSc={selectedSc}
+            setSelectedSc={setSelectedSc}
           />
         )}
 
@@ -64,16 +79,25 @@ const PlannerPage = () => {
             weekFound={calendar.weekFound}
             //weekNames={calendar.weekNames}
             month={calendar.month}
-            // searchQuery={searchQuery}
-            // setSearchQuery={setSearchQuery}
-            /// categoryFilter={categoryFilter}
-            // setCategoryFilter={setCategoryFilter}
-            //   onPrevMonth={handlePrevMonth}
-            //  onNextMonth={handleNextMonth}
-            //  onToday={handleToday}
-            //  onAddEvent={handleAddEvent}
+            openScModal={openScModal}
+            setOpenScModal={setOpenScModal}
+            selectedSc={selectedSc}
+            setSelectedSc={setSelectedSc}
+            nowPlanner={nowPlanner}
+            setNowPlanner={setNowPlanner}
           />
         </div>
+
+        {/**여기에 그 뭐냐 그룹 플래너 사이드바 들어가면 될 것 같다는...아닌가..대가리가 아픈 것 같다는 */}
+        {openScModal && (
+          <ScheduleModal
+            openScModal={openScModal}
+            setOpenScModal={setOpenScModal}
+            setSelectedSc={setSelectedSc}
+            selectedSc={selectedSc}
+            scFilter={scheduleFilter}
+          />
+        )}
       </div>
     </div>
   );
