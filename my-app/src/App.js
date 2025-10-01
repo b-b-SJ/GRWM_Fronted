fcd7a132cbd512141506818992d7e50afee7a259
Lagufiesta <leesoos728@gmail.com>
2025-10-01 09:13:49 +0900

    merge 충돌 해결

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import AppLayout from "./components/layout/AppLayout";
 import MainPage from "./components/pages/MainPage";
 import PlannerPage from "./components/pages/PlannerPage";
 import TrackerPage from "./components/pages/TrackerPage";
-import CommunityPage from "./components/pages/CommunityPage";
+import CommunityPage from "./components/pages/community/CommunityPage";
 import WorkspacePage from "./components/pages/WorkspacePage";
 import { ChatStateProvider } from "./hooks/useChatState";
 import AuthPage from "./components/pages/auth/AuthPage";
@@ -31,7 +31,7 @@ const App = () => {
               <Route path="planner" element={<PlannerPage />} />
               <Route path="tracker" element={<TrackerPage />} />
               <Route path="workspace/*" element={<WorkspacePage />} />
-              <Route path="community" element={<CommunityPage />} />
+              <Route path="community/*" element={<CommunityPage />} />
             </Route>
 
             <Route path="*" element={<Navigate to="/auth" replace />} />
