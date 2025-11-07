c3f5527ef07c1fc3d0fa754a174416888486f620
KSJ <130431609+b-b-SJ@users.noreply.github.com>
2025-11-07 12:06:01 +0900

    App.js 구조 수정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import AppLayout from './components/layout/AppLayout';
-import MainPage from './components/pages/MainPage';
-import PlannerPage from './components/pages/PlannerPage';
-import TrackerPage from './components/pages/TrackerPage';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AppLayout from "./components/layout/AppLayout";
+import MainPage from "./components/pages/MainPage";
+import PlannerPage from "./components/pages/PlannerPage";
+import TrackerPage from "./components/pages/TrackerPage";
 import CommunityPage from "./components/pages/community/CommunityPage";
 import WorkspacePage from './components/pages/WorkspacePage';
 import { ChatStateProvider } from './hooks/useChatState';
@@ -12,8 +12,8 @@ import { StudyRoomWebSocketProvider } from './hooks/StudyRoomWebSocketContext';
 import AuthPage from './components/pages/auth/AuthPage';
 import { AuthProvider } from './hooks/AuthContext';
 import ProtectedRoute from "./components/ProtectedRoute";
-import { TeamPlannerProvider } from './hooks/TeamPlannerProvider';
-import TeamPlannerTestPage from './components/planner/groupPlanner/TeamPlannerTestPage'; // 공유 플래너 테스트 페이지 (추후 삭졔)
+import { TeamPlannerProvider } from "./hooks/TeamPlannerProvider";
+import TeamPlannerTestPage from "./components/planner/groupPlanner/TeamPlannerTestPage"; // 공유 플래너 테스트 페이지 (추후 삭졔)
 
 const App = () => {
     return (
@@ -21,25 +21,27 @@ const App = () => {
             <BrowserRouter>
                 <WebSocketProvider>
                     <StudyRoomWebSocketProvider>
-                    <ChatStateProvider>
-                        <TeamPlannerProvider>
-                            <Routes>
-                                <Route path="auth" element={<AuthPage />} />
-                                <Route path="/" element={<Navigate to="/auth" replace />} />
-
-                                <Route element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
-                                    <Route path="main" element={<MainPage />} />
-                                    <Route path="planner/*" element={<PlannerPage />} />
-                                    <Route path="planner/shared" element={<TeamPlannerTestPage />} />
-                                    <Route path="tracker" element={<TrackerPage />} />
-                                    <Route path="workspace/*" element={<WorkspacePage />} />
-                                    <Route path="community" element={<CommunityPage />} />
-                                </Route>
-
-                                <Route path="*" element={<Navigate to="/auth" replace />} />
-                            </Routes>
-                        </TeamPlannerProvider>
-                    </ChatStateProvider>
+                        <ChatStateProvider>
+                            <TeamPlannerProvider>
+                                <Routes>
+                                    <Route path="auth" element={<AuthPage />} />
+                                    <Route path="/" element={<Navigate to="/auth" replace />} />
+                                    <Route element={
+                                        <ProtectedRoute>
+                                            <AppLayout />
+                                        </ProtectedRoute>
+                                    }>
+                                        <Route path="main" element={<MainPage />} />
+                                        <Route path="planner/*" element={<PlannerPage />} />
+                                        <Route path="planner/shared" element={<TeamPlannerTestPage />}/>
+                                        <Route path="tracker" element={<TrackerPage />} />
+                                        <Route path="workspace/*" element={<WorkspacePage />} />
+                                        <Route path="community/*" element={<CommunityPage />} />
+                                    </Route>
+                                    <Route path="*" element={<Navigate to="/auth" replace />} />
+                                </Routes>
+                            </TeamPlannerProvider>
+                        </ChatStateProvider>
                     </StudyRoomWebSocketProvider>
                 </WebSocketProvider>
             </BrowserRouter>
